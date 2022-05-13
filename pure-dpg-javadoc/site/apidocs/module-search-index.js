6596113a030c61f92ddb77648f01579fbf0cbd5c
Haoling Dong <haolingdong@microsoft.com>
2022-05-13 11:58:00 +0800

    Add Javadoc

---
+++ pure-dpg-javadoc/site/apidocs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.azure.ai.metricsadvisor"}];updateSearchResults();
---
+++ pure-dpg-javadoc/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.azure.ai.metricsadvisor","l":"com.azure.ai.metricsadvisor"}];updateSearchResults();
